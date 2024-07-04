d3dcacf00172cc48b3132c68d169109ca2bf866c
Rohit Rajvaidya <rohitcode25@gmail.com>
2024-07-04 21:04:00 +0530

    Added Assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,57 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  LinkedinIcon,
+  LinkedinShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+  WhatsappIcon,
+} from "react-share";
+import { useState, useEffect, useRef } from "react";
 
 function App() {
+ 
   const [quote, setQuote] = useState("Just Do It");
   const [character, setCharacter] = useState("Character");
   const [anime, setAnime] = useState("Anime");
   const [randomNumber, setRandomNumber] = useState(null);
-  const [url , seturl] = useState("")
+
+  const bgUrlRef = useRef(null);
+  const xUrlRef = useRef(`https://twitter.com/intent/tweet?text=${quote}&hashtags=[hashtags]`);
+  const linkedinUrlRef = useRef(`https://www.linkedin.com/shareArticle?mini=true&url=http://google.com&title=${quote}&summary=${quote} - ${character} (${anime})&source=LinkedIn`);
 
   const fetchQuote = (number) => {
-    fetch('/Anime-Quotes.json')
-      .then(response => response.json())
-      .then(data => {
+    fetch("/Anime-Quotes.json")
+      .then((response) => response.json())
+      .then((data) => {
         if (data[number]) {
           const randomQuote = data[number];
           setQuote(randomQuote.quote);
           setCharacter(randomQuote.character);
           setAnime(randomQuote.anime);
         } else {
-          console.error('No quote found at index:', number);
+          console.error("No quote found at index:", number);
         }
       })
-      .catch(error => console.error('Error fetching the JSON:', error));
+      .catch((error) => console.error("Error fetching the JSON:", error));
   };
 
   const randomQuote = () => {
     const number = Math.floor(Math.random() * 45);
     setRandomNumber(number);
+    bgUrlRef.current.style.backgroundImage = "url('./assets/jiraya.png')";
   };
+
+  useEffect(() => {
+    xUrlRef.current.href = `https://twitter.com/intent/tweet?text=This quote is shared from "Anime-Quoter"%0a${quote}%0a-${character} %0adownload extension now !!!%0a&hashtags=animequote`;
+  });
+
+  useEffect(() => {
+    linkedinUrlRef.current.href = `https://www.linkedin.com/shareArticle?mini=true&url=http://developer.linkedin.com&title="This is the title"&summary=This is the summary &source=This is the source`;
+  });
 
   useEffect(() => {
     if (randomNumber !== null) {
@@ -39,50 +63,93 @@ function App() {
   }, [randomNumber]);
 
   return (
-    <>
-      <div className="first-section w-full text-gray-100 h-[15vh] p-2 flex justify-between items-center">
-        <div className="flex items-center gap-2">
+    <div className="main-container custom-class @apply text-[white] text-center overflow-hidden relative m-0 before:content-[''] before:absolute before:w-full before:h-full before:bg-[url('./assets/jiraya.png')] before:bg-cover before:bg-center before:z-[-2] before:grayscale-[80%] before:left-0 before:top-0 after:content-[''] after:absolute after:w-full after:h-full after:mix-blend-multiply after:opacity-70 after:z-[-1] after:left-0 after:top-0; h-[94vh] md:h-full
+  font-family: Arial, sans-serif;" ref={bgUrlRef}>
+      <div className="first-section w-full text-white h-[8vh] md:h-[15vh] p-2 flex justify-between items-center relative">
+        <div className="background-overlay absolute top-0 left-0 w-full h-full bg-black opacity-40"></div>
+        <div className="flex items-center gap-2 relative z-10">
           <img
             src="./src/assets/logo.jpg"
-            className="w-[3vw] md:w-12 rounded-full"
+            className="w-[10vw] md:w-[4vw] rounded-full"
             alt="Logo"
           />
-          <div className="logo font-medium text-xl">Anime Quote</div>
+          <div className="bangers-regular logo font-medium text-2xl md:text-4xl">
+            Anime Quote
+          </div>
         </div>
 
-        <div className="flex gap-5 items-center">
-          <div className="p-2">Share On:</div>
-          <a className="px-2 py-2 cursor-pointer border border-gray-500 rounded-xl hover:bg-white hover:text-black" href="https://twitter.com/intent/tweet?text=[text to share]&hashtags=[hashtags]" target="blank">Twitter</a>
-          <button className="p-2 border border-gray-500 rounded-xl hover:bg-white hover:text-black">Facebook</button>
-          <button className="p-2 border border-gray-500 rounded-xl hover:bg-white hover:text-black">Dark Mode</button>
+        <div className="flex gap-5 mr-5 items-center relative z-10">
+          <div className="p-2"></div>
+          <a
+            className="px-2 py-2 cursor-pointer border border-gray-500 rounded-xl hover:bg-black hover:text-black"
+            ref={xUrlRef}
+            target="blank"
+          >
+            <img src="./src/assets/x-white.png" className="w-4" alt="x-logo" />
+          </a>
+          
+          <a
+            ref={linkedinUrlRef}
+            target="blank"
+            className="p-2 border border-gray-500 rounded-xl hover:bg-blue-800 hover:text-black"
+          >
+            <img
+              src="./src/assets/linkedin-white.png"
+              alt="linkedin-logo"
+              className="w-4"
+            />
+          </a>
+          
         </div>
       </div>
 
       <div className="middle-section w-full h-[75vh] flex flex-col justify-center items-center">
-        <div className="h-[70vh] w-[90vw] md:w-[80vw]">
-          <div className="flex flex-col w-full h-[50vh] rounded-2xl justify-center items-center">
-            <div className="text-2xl md:text-3xl font-sans font-bold text-center px-2">{quote}</div>
+        <div className="">
+          <div className="flex flex-col w-full h-[50vh] justify-center items-center">
+            <div className="text-2xl md:text-3xl font-sans font-bold text-center px-10">
+              {quote}
+            </div>
           </div>
-          <div className="flex flex-col md:flex-row justify-between w-full h-[10vh] px-4 md:px-20">
-            <div className="self-end md:self-center text-base">
-              Character: <span className=" text-lg">{character}</span>
+          <div className="">
+            <div className="">
+            - <span className=" ">{character}</span>
             </div>
             <div className="self-end md:self-center text-base">
-              Anime: <span className="text-lg">{anime}</span>
+              <span className="font-bold">Anime :</span><span className="text-lg">{anime}</span>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="third-section flex flex-col md:flex-row justify-between items-center p-5 h-auto md:h-[10vh]">
-        <div className="flex gap-5 mb-3 md:mb-0">
-          <div>Settings</div>
-          <button>i Button</button>
-          <button onClick={randomQuote} className="px-3 border border-gray-500 rounded-xl hover:bg-white hover:text-black">New Quote</button>
+      <div className="third-section text-sm relative flex md:flex-row justify-between items-center p-5 h-auto md:h-[10vh] md:flex md:gap-5 md:text-lg">
+        <div className="background-overlay absolute top-0 left-0 w-full h-full bg-black opacity-40"></div>
+
+        <div className="flex gap-5 mb-3 md:mb-0 z-10 md:gap-5 items-center justify-between">
+          <button
+          className="p-2 md:px-3 border border-gray-500 rounded-xl hover:bg-white hover:text-black md:text-sm text-sm"
+          >Settings</button>
+          <button
+            onClick={randomQuote}
+            className="p-2 md:px-3 border border-gray-500 rounded-xl hover:bg-white hover:text-black md:text-sm text-xs"
+          >
+            New Quote
+          </button>
+          <div>
+            <Popup 
+            trigger=
+                {<button className="bg-white h-7 w-7 text-black font-bold text-xl font-mono rounded-2xl hover:bg-blue-400">i</button>}
+                position="top center">
+                
+                <button className=" flex justify-center text-center text-white items-center gap-2 w-10vw">
+                  <span><img src="./src/assets/github-white.png" className="w-5" alt="" /></span>
+                  <a href="https://github.com/RohitRajvaidya5/Anime-Quote">Github</a>
+                </button>
+            </Popup>
         </div>
-        <div>Want to suggest a quote?</div>
+        </div>
+        <button className="relative z-10 px-3 text-gray-200 hover:text-white">Suggest a quote?</button>
       </div>
-    </>
+    </div>
   );
 }
 
